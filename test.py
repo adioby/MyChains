0135926a035389b2078034081e145387e4259e9f
adioby <adioby@gmail.com>
2021-10-21 15:05:58 +0000

    Mise à jour de la classe myChains Changement du nom du fichier de la classe a myChains.py ajout de la function search

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-from myChains import myChains, myChain, valide_file
+from myChains import myChains, myChain, valide_file,listeToString
 
 x = "self.__Nom\nself.__Prenoms\nself.__CodeUser\nself.__Matricule \nself.__CAI\nself.__Sexe\nself.__Telephone\nself.__Email "
-y = "=Nom\n=Prenoms\n=CodeUser\n=Matriculate\n=CAI\n=Sexe\n=Telephone\n=Email"
+y = "om\nPrenom" #"\n=CodeUser\n=Matriculate\n=CAI\n=Sexe\n=Telephone\n=Email"
+z=["om","nom"]
 
 f1 = "fred1.txt"
 f2 = "fred2.txt"
 nom_fic = "fred3.txt"
-f4 = "myChains.py"
-f5="C:\Temp\jde_log\ACS.splf-331176694244807445.txt"
-f6="C:\Temp\jde_log\ACS.splf-3204451050875517401.txt"
-search="Total size :"
+# f4 = "myChains.py"
+# f5="C:\Temp\jde_log\ACS.splf-331176694244807445.txt"
+# f6="C:\Temp\jde_log\ACS.splf-3204451050875517401.txt"
+# search="Total size :"
 
-ch=myChains(f6,search)
-print(ch.find())
+ch=myChains(x,listeToString(z)).find()
+print(ch)
